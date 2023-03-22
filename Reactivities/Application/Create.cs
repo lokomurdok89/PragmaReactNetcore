6663af9ac5078850241e380ef886d56b7d32d6f9
Daniel Toledo <Daniel.Toledo@belltech.la>
2023-03-22 16:26:08 -0300

    se genera crud

--- Reactivities/Application/Create.cs
+++ Reactivities/Application/Create.cs
@@ -1,12 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
 
 namespace Application
 {
     public class Create
     {
+        public class Command: IRequest{
+            public User  user { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;                
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Users.Add(request.user);// use addAsync when go to DB, this example is in memory
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
         
     }
 }
